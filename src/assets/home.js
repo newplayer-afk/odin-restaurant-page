84c57f3683385e9fd61264517e73a011870476b6
seanaelius <sdc.startup@gmail.com>
2024-10-08 18:06:58 -0400

    successfully completed test 1

--- src/assets/home.js
+++ src/assets/home.js
@@ -1,12 +1,12 @@
 export {home}; 
 
 const home = (function() {
-
     const add = () => {
+        const mainContent = document.querySelector("#content")
         const testDiv = document.createElement("div")
-        testDiv.classList.add("test1")
-        mainContent.textContent = "hi"
+        testDiv.classList.add("test")
+        testDiv.textContent = "hi"
+        mainContent.appendChild(testDiv)
     }  
-    
     return {add}
 })();
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 import "./styles.css";
 import { home } from "./assets/home.js"
 
-mainContent = document.querySelector("#content")
+const mainContent = document.querySelector("#content")
 
-const op1 = document.querySelector("home")
-op1.addEventListener("click", () => {home.add()})
+const op1 = document.querySelector("#home")
+op1.addEventListener("click", () => {
+    home.add()
+})
